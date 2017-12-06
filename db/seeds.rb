15821ca86eebebf0577a185055f312416120b904
xavierwalker <xwalk91@gmail.com>
2017-12-05 19:03:40 -0500

    added first product to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+gammaBomb = Product.create(
+  title: "The Incredible Hulk",
+  subtitle: "issue 103",
+  image_name: "incredible_hulk103.jpg",
+  price: "4.99",
+  sku: "IH103",
+  info: "In ancient times he was worshipped as a god; he has not forgotten this.",
+  download_url: "https://s3.us-east-2.amazonaws.com/comic-app/incredible_hulk103.jpg",
+  details: "One of the most iconic characters in popular culture.",
+  description: %{<p>The 1990s saw the Green Hulk return.</p>})
