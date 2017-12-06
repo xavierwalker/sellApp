e1e337a8d718b6d1c63e99e0425541b801c08c98
xavierwalker <xwalk91@gmail.com>
2017-12-05 21:05:29 -0500

    added sub plan for hulk

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,17 +6,11 @@ class ChargesController < ApplicationController
 
 		  customer = Stripe::Customer.create(
 		    :email => params[:stripeEmail],
-		    :source  => params[:stripeToken]
+		    :source  => params[:stripeToken],
+		    :plan => "HULK"
 		  )
 
-		  charge = Stripe::Charge.create(
-		    :customer    => customer.id,
-		    :amount      => product.price_in_cents,
-		    :description => 'Rails Stripe customer',
-		    :currency    => 'usd'
-		  )
-
-			purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], amount: product.price_in_cents, description: charge.description, currency: charge.currency, customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
+			purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], amount: product.price_in_cents, description: product.description, currency: "usd", customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
 
 			redirect_to purchase
 
